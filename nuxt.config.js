7173470203b70a66af499f9b399a9f2cd80d90a1
MikeDre <info@mikedre.com>
2020-07-05 10:56:26 +0100

    [master] Base page build

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,21 +14,27 @@ export default {
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
   */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
+    head: {
+        title: 'Birdlife',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/png', href: '/icon.png' },
+            { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.5/css/uikit.min.css' }
+        ],
+        script: [
+            { src: 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.5/js/uikit.min.js' },
+            { src: 'https://kit.fontawesome.com/bddb4fd396.js' }
+        ],
+    },
   /*
   ** Global CSS
   */
   css: [
+    './assets/sass/main.scss'
   ],
   /*
   ** Plugins to load before mounting the App
@@ -50,7 +56,16 @@ export default {
   ** Nuxt.js modules
   */
   modules: [
+    '@nuxtjs/style-resources',
   ],
+  /*
+  ** Style Resources configuration
+  */
+  styleResources: {
+    scss: [
+      './assets/sass/main.scss'
+    ]
+  },
   /*
   ** Build configuration
   ** See https://nuxtjs.org/api/configuration-build/
