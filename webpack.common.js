044a557d58963ed7d429b645e1b28dd851a4a3ca
MikeDre <info@mikedre.com>
2020-07-06 09:04:26 +0100

    [master] Fixed webpack/paths issue

--- webpack.common.js
+++ webpack.common.js
@@ -2,38 +2,40 @@ const path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: {
-    main: "./src/index.js",
-    // vendor: "./src/vendor.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: ["html-loader"]
-      },
-      {
-        test: /\.(svg|png|jpg|gif)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "[name].[hash].[ext]",
-            outputPath: "imgs"
-          }
-        }
-      },
-    {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
+    entry: {
+        main: "./src/index.js",
+        // vendor: "./src/vendor.js"
+    },
+    module: {
+        rules: [
             {
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]',
-                    outputPath: 'fonts'
-                }
-            }
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[hash].[ext]',
+                            outputPath: 'imgs'
+                        }
+                    }
+                ]
+            },
         ]
     }
-    ]
-  }
 };
